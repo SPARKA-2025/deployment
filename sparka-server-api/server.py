295917b3d8da70e5f16e999b3389323ac41f301b
Ahmad Zein Al Wafi <drakeequehours@gmail.com>
2024-09-26 18:10:44 +0700

    add notebook

--- sparka-server-api/server.py
+++ sparka-server-api/server.py
@@ -304,5 +304,12 @@ def get_performance():
         'score': score
     })
 
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({
+        'message': "the server are run correctly",
+        'status': "200"
+    })
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
