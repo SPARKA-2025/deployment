6ff0cece66cd193cc8afea2a83da9f05a1f14bc7
Ahmad Zein Al Wafi <drakeequehours@gmail.com>
2024-10-26 02:52:17 +0700

    add: navbar to login page dashboard nextjs

--- http/dashboard-nextjs/app/login/page.tsx
+++ http/dashboard-nextjs/app/login/page.tsx
@@ -2,19 +2,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const handleSubmit = async (e : any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
       const response = await axios.post('/api/login', { username, password });
-      console.log(response.data.success)
+      console.log(response.data.success);
       if (response.data.success) {
         console.log('Login successful');
         router.push('/dashboard');
@@ -27,47 +28,59 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
-        <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">Login</h2>
-        <h3 className="text-2xl font-bold mb-6 text-center text-gray-700">
-          Plate Detection Operations Dashboard
-        </h3>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-600">
-              Username
-            </label>
-            <input
-              id="username"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-600">
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            />
-          </div>
-          {error && <p className="text-red-500 text-sm">{error}</p>}
-          <button
-            type="submit"
-            className="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          >
-            Login
-          </button>
-        </form>
+    <div className="font-sans bg-gray-50">
+      {/* Navbar */}
+      <nav className="flex justify-around bg-gray-800 text-white p-4">
+        <Link href="/">Home</Link>
+        <Link href="#overview">Overview</Link>
+        <Link href="#features">Features</Link>
+        <Link href="/docs">Documentation</Link>
+        <Link href="#team">Team</Link>
+        <Link href="/login">Login Dashboard</Link>
+      </nav>
+
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
+          <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">Login</h2>
+          <h3 className="text-2xl font-bold mb-6 text-center text-gray-700">
+            Plate Detection Operations Dashboard
+          </h3>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label htmlFor="username" className="block text-sm font-medium text-gray-600">
+                Username
+              </label>
+              <input
+                id="username"
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-600">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+            </div>
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            >
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
